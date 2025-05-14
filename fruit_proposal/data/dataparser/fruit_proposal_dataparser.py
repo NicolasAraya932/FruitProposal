4ac9f54aa57971fbf26f8f959645026aff13b2a5
Nicolás Araya <63214589+NicolasAraya932@users.noreply.github.com>
2025-05-14 17:13:13 -0400

    Preparing DataParser Scripts

--- fruit_proposal/data/dataparser/fruit_proposal_dataparser.py
+++ fruit_proposal/data/dataparser/fruit_proposal_dataparser.py
@@ -26,7 +26,12 @@ from PIL import Image
 from nerfstudio.cameras import camera_utils
 from nerfstudio.cameras.cameras import CAMERA_MODEL_TO_TYPE, Cameras, CameraType
 from nerfstudio.data.dataparsers.base_dataparser import DataParser, DataParserConfig
+from nerfstudio.data.dataparsers.nerfstudio_dataparser import NerfstudioDataParserConfig
+
+
 from fruit_proposal.data.dataparser.fruit_proposal_base_dataparser import FruitProposalDataparserOutputs
+
+
 from nerfstudio.data.scene_box import SceneBox
 from nerfstudio.data.utils.dataparsers_utils import (
     get_train_eval_split_all,
@@ -41,43 +46,11 @@ MAX_AUTO_RESOLUTION = 1600
 
 
 @dataclass
-class FruitProposalDataparserConfig(DataParserConfig):
+class FruitProposalDataparserConfig(NerfstudioDataParserConfig):
     """Nerfstudio dataset config"""
 
     _target: Type = field(default_factory=lambda: FruitProposalDataparser)
     """target class to instantiate"""
-    data: Path = Path()
-    """Directory or explicit json file path specifying location of data."""
-    scale_factor: float = 1.0
-    """How much to scale the camera origins by."""
-    downscale_factor: Optional[int] = None
-    """How much to downscale images. If not set, images are chosen such that the max dimension is <1600px."""
-    scene_scale: float = 1.0
-    """How much to scale the region of interest by."""
-    orientation_method: Literal["pca", "up", "vertical", "none"] = "up"
-    """The method to use for orientation."""
-    center_method: Literal["poses", "focus", "none"] = "poses"
-    """The method to use to center the poses."""
-    auto_scale_poses: bool = True
-    """Whether to automatically scale the poses to fit in +/- 1 bounding box."""
-    eval_mode: Literal["fraction", "filename", "interval", "all"] = "fraction"
-    """
-    The method to use for splitting the dataset into train and eval.
-    Fraction splits based on a percentage for train and the remaining for eval.
-    Filename splits based on filenames containing train/eval.
-    Interval uses every nth frame for eval.
-    All uses all the images for any split.
-    """
-    train_split_fraction: float = 0.9
-    """The percentage of the dataset to use for training. Only used when eval_mode is train-split-fraction."""
-    eval_interval: int = 8
-    """The interval between frames to use for eval. Only used when eval_mode is eval-interval."""
-    depth_unit_scale_factor: float = 1e-3
-    """Scales the depth values to meters. Default value is 0.001 for a millimeter to meter conversion."""
-    mask_color: Optional[Tuple[float, float, float]] = None
-    """Replace the unknown pixels with this color. Relevant if you have a mask but still sample everywhere."""
-    load_3D_points: bool = False
-    """Whether to load the 3D points from the colmap reconstruction."""
 
 
 @dataclass
