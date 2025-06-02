c8da4e02f7cc86b524b7ee88af30699ae5546ec5
Nicolás Araya <63214589+NicolasAraya932@users.noreply.github.com>
2025-06-02 00:53:47 -0400

    To try: Using Vanilla NeRF to merge fields

--- fruit_proposal/fruit_proposal_field.py
+++ fruit_proposal/fruit_proposal_field.py
@@ -24,23 +24,24 @@ from torch import Tensor, nn
 from nerfstudio.cameras.rays import RaySamples
 from nerfstudio.data.scene_box import SceneBox
 from nerfstudio.field_components.activations import trunc_exp
+from nerfstudio.field_components.encodings import Encoding, Identity
 from nerfstudio.field_components.embedding import Embedding
-from nerfstudio.field_components.encodings import NeRFEncoding, SHEncoding
+from nerfstudio.field_components.encodings import HashEncoding, NeRFEncoding, SHEncoding
 from nerfstudio.field_components.field_heads import (
     FieldHeadNames,
-    PredNormalsFieldHead,
+    DensityFieldHead,
     SemanticFieldHead,
-    TransientDensityFieldHead,
-    TransientRGBFieldHead,
-    UncertaintyFieldHead,
+    RGBFieldHead
 )
-from nerfstudio.field_components.mlp import MLP, MLPWithHashEncoding
+from nerfstudio.field_components.mlp import MLP
 from nerfstudio.field_components.spatial_distortions import SpatialDistortion
-from nerfstudio.fields.base_field import Field, get_normalized_directions
+from nerfstudio.fields.base_field import Field, shift_directions_for_tcnn
+
+from fruit_nerf.components.field_heads import SemanticFieldHead
 
 
 class FruitProposalField(Field):
-    """Compound Field
+    """Compound Field that uses TCNN
 
     Args:
         aabb: parameters of scene aabb bounds
@@ -49,17 +50,14 @@ class FruitProposalField(Field):
         hidden_dim: dimension of hidden layers
         geo_feat_dim: output geo feat dimensions
         num_levels: number of levels of the hashmap for the base mlp
-        base_res: base resolution of the hashmap for the base mlp
         max_res: maximum resolution of the hashmap for the base mlp
         log2_hashmap_size: size of the hashmap for the base mlp
         num_layers_color: number of hidden layers for color network
         num_layers_transient: number of hidden layers for transient network
-        features_per_level: number of features per level for the hashgrid
         hidden_dim_color: dimension of hidden layers for color network
         hidden_dim_transient: dimension of hidden layers for transient network
         appearance_embedding_dim: dimension of appearance embedding
         transient_embedding_dim: dimension of transient embedding
-        use_transient_embedding: whether to use transient embedding
         use_semantics: whether to use semantic segmentation
         num_semantic_classes: number of semantic classes
         use_pred_normals: whether to use predicted normals
@@ -70,32 +68,30 @@ class FruitProposalField(Field):
     aabb: Tensor
 
     def __init__(
-        self,
-        aabb: Tensor,
-        num_images: int,
-        num_layers: int = 2,
-        hidden_dim: int = 64,
-        geo_feat_dim: int = 15,
-        num_levels: int = 16,
-        base_res: int = 16,
-        max_res: int = 2048,
-        log2_hashmap_size: int = 19,
-        num_layers_color: int = 3,
-        num_layers_transient: int = 2,
-        features_per_level: int = 2,
-        hidden_dim_color: int = 64,
-        hidden_dim_transient: int = 64,
-        appearance_embedding_dim: int = 32,
-        transient_embedding_dim: int = 16,
-        use_transient_embedding: bool = False,
-        use_semantics: bool = False,
-        num_semantic_classes: int = 2,
-        pass_semantic_gradients: bool = False,
-        use_pred_normals: bool = False,
-        use_average_appearance_embedding: bool = False,
-        spatial_distortion: Optional[SpatialDistortion] = None,
-        average_init_density: float = 1.0,
-        implementation: Literal["tcnn", "torch"] = "tcnn",
+            self,
+            aabb: Tensor,
+            num_images: int,
+            num_layers: int = 2,
+            hidden_dim: int = 64,
+            geo_feat_dim: int = 15,
+            num_levels: int = 16,
+            base_res: int = 16,
+            max_res: int = 2048,
+            log2_hashmap_size: int = 19,
+            num_layers_color: int = 3,
+            num_layers_semantic: int = 2,
+            features_per_level: int = 2,
+            hidden_dim_color: int = 64,
+            hidden_dim_semantics: int = 64,
+            hidden_dim_transient: int = 64,
+            appearance_embedding_dim: int = 32,
+            use_semantics: bool = False,
+            test_mode: str = None,
+            num_semantic_classes: int = 100,
+            pass_semantic_gradients: bool = False,
+            use_average_appearance_embedding: bool = False,
+            spatial_distortion: Optional[SpatialDistortion] = None,
+            implementation: Literal["tcnn", "torch"] = "tcnn",
     ) -> None:
         super().__init__()
 
@@ -109,18 +105,12 @@ class FruitProposalField(Field):
         self.spatial_distortion = spatial_distortion
         self.num_images = num_images
         self.appearance_embedding_dim = appearance_embedding_dim
-        if self.appearance_embedding_dim > 0:
-            self.embedding_appearance = Embedding(self.num_images, self.appearance_embedding_dim)
-        else:
-            self.embedding_appearance = None
+        self.embedding_appearance = Embedding(self.num_images, self.appearance_embedding_dim)
         self.use_average_appearance_embedding = use_average_appearance_embedding
-        self.use_transient_embedding = use_transient_embedding
         self.use_semantics = use_semantics
-        self.use_pred_normals = use_pred_normals
+        self.test_mode = test_mode
         self.pass_semantic_gradients = pass_semantic_gradients
         self.base_res = base_res
-        self.average_init_density = average_init_density
-        self.step = 0
 
         self.direction_encoding = SHEncoding(
             levels=4,
@@ -131,13 +121,16 @@ class FruitProposalField(Field):
             in_dim=3, num_frequencies=2, min_freq_exp=0, max_freq_exp=2 - 1, implementation=implementation
         )
 
-        """BASE MLP"""
-        self.mlp_base = MLPWithHashEncoding(
+        self.mlp_base_grid = HashEncoding(
             num_levels=num_levels,
             min_res=base_res,
             max_res=max_res,
             log2_hashmap_size=log2_hashmap_size,
             features_per_level=features_per_level,
+            implementation=implementation,
+        )
+        self.mlp_base_mlp = MLP(
+            in_dim=self.mlp_base_grid.get_out_dim(),
             num_layers=num_layers,
             layer_width=hidden_dim,
             out_dim=1 + self.geo_feat_dim,
@@ -145,50 +138,22 @@ class FruitProposalField(Field):
             out_activation=None,
             implementation=implementation,
         )
-
-        # transients
-        if self.use_transient_embedding:
-            self.transient_embedding_dim = transient_embedding_dim
-            self.embedding_transient = Embedding(self.num_images, self.transient_embedding_dim)
-            self.mlp_transient = MLP(
-                in_dim=self.geo_feat_dim + self.transient_embedding_dim,
-                num_layers=num_layers_transient,
-                layer_width=hidden_dim_transient,
-                out_dim=hidden_dim_transient,
-                activation=nn.ReLU(),
-                out_activation=None,
-                implementation=implementation,
-            )
-            self.field_head_transient_uncertainty = UncertaintyFieldHead(in_dim=self.mlp_transient.get_out_dim())
-            self.field_head_transient_rgb = TransientRGBFieldHead(in_dim=self.mlp_transient.get_out_dim())
-            self.field_head_transient_density = TransientDensityFieldHead(in_dim=self.mlp_transient.get_out_dim())
+        self.mlp_base = torch.nn.Sequential(self.mlp_base_grid, self.mlp_base_mlp)
 
         # semantics
-        self.mlp_semantics = MLP(
-            in_dim=self.geo_feat_dim,
-            num_layers=2,
-            layer_width=64,
-            out_dim=num_semantic_classes,
-            activation=nn.ReLU(),
-            out_activation=None,
-            implementation=implementation,
-        )
-        self.field_head_semantics = SemanticFieldHead(
-            in_dim=self.mlp_semantics.get_out_dim(), num_classes=num_semantic_classes
-        )
-
-        # predicted normals
-        if self.use_pred_normals:
-            self.mlp_pred_normals = MLP(
-                in_dim=self.geo_feat_dim + self.position_encoding.get_out_dim(),
-                num_layers=3,
-                layer_width=64,
+        if self.use_semantics:
+            self.mlp_semantics = MLP(
+                in_dim=self.geo_feat_dim,
+                num_layers=num_layers_semantic, #num_layers=2,
+                layer_width=hidden_dim_semantics, #layer_width=64,
                 out_dim=hidden_dim_transient,
                 activation=nn.ReLU(),
                 out_activation=None,
                 implementation=implementation,
             )
-            self.field_head_pred_normals = PredNormalsFieldHead(in_dim=self.mlp_pred_normals.get_out_dim())
+            self.field_head_semantics = SemanticFieldHead(
+                in_dim=self.mlp_semantics.get_out_dim(), num_classes=num_semantic_classes, activation=None
+            )
 
         self.mlp_head = MLP(
             in_dim=self.direction_encoding.get_out_dim() + self.geo_feat_dim + self.appearance_embedding_dim,
@@ -208,18 +173,14 @@ class FruitProposalField(Field):
             positions = (positions + 2.0) / 4.0
         else:
             positions = SceneBox.get_normalized_positions(ray_samples.frustums.get_positions(), self.aabb)
+
         # Make sure the tcnn gets inputs between 0 and 1.
         selector = ((positions > 0.0) & (positions < 1.0)).all(dim=-1)
         positions = positions * selector[..., None]
-
-        assert positions.numel() > 0, "positions is empty."
-
         self._sample_locations = positions
         if not self._sample_locations.requires_grad:
             self._sample_locations.requires_grad = True
         positions_flat = positions.view(-1, 3)
-
-        assert positions_flat.numel() > 0, "positions_flat is empty."
         h = self.mlp_base(positions_flat).view(*ray_samples.frustums.shape, -1)
         density_before_activation, base_mlp_out = torch.split(h, [1, self.geo_feat_dim], dim=-1)
         self._density_before_activation = density_before_activation
@@ -227,53 +188,76 @@ class FruitProposalField(Field):
         # Rectifying the density with an exponential is much more stable than a ReLU or
         # softplus, because it enables high post-activation (float32) density outputs
         # from smaller internal (float16) parameters.
-        density = self.average_init_density * trunc_exp(density_before_activation.to(positions))
+        density = trunc_exp(density_before_activation.to(positions))
         density = density * selector[..., None]
         return density, base_mlp_out
 
+    def get_inference_outputs(
+            self, ray_samples: RaySamples, density_embedding: Optional[Tensor] = None, render_rgb: bool = False
+    ) -> Dict[FieldHeadNames, Tensor]:
+
+        outputs = {}
+        # semantics
+        semantics_input = density_embedding.view(-1, self.geo_feat_dim)
+        if not self.pass_semantic_gradients:
+            semantics_input = semantics_input.detach()
+
+        x = self.mlp_semantics(semantics_input).view(*ray_samples.shape, -1).to(semantics_input).to(torch.float32)
+        outputs[FieldHeadNames.SEMANTICS] = self.field_head_semantics(x)
+
+        directions = shift_directions_for_tcnn(ray_samples.frustums.directions)
+        directions_flat = directions.view(-1, 3)
+        d = self.direction_encoding(directions_flat)
+        outputs_shape = ray_samples.frustums.directions.shape[:-1]
+
+        # embedded_appearance = torch.zeros(
+        #    (*directions.shape[:-1], self.appearance_embedding_dim), device=directions.device
+        # )
+
+        embedded_appearance = torch.ones(
+            (*directions.shape[:-1], self.appearance_embedding_dim), device=directions.device
+        ) * self.embedding_appearance.mean(dim=0)
+
+        h = torch.cat(
+            [
+                d,
+                density_embedding.view(-1, self.geo_feat_dim),
+                embedded_appearance.view(-1, self.appearance_embedding_dim),
+            ],
+            dim=-1,
+        )
+        rgb = self.mlp_head(h).view(*outputs_shape, -1).to(directions)
+        outputs.update({FieldHeadNames.RGB: rgb})
+
+        return outputs
+
     def get_outputs(
-        self, ray_samples: RaySamples, density_embedding: Optional[Tensor] = None
+            self, ray_samples: RaySamples, density_embedding: Optional[Tensor] = None
     ) -> Dict[FieldHeadNames, Tensor]:
         assert density_embedding is not None
+
         outputs = {}
         if ray_samples.camera_indices is None:
             raise AttributeError("Camera indices are not provided.")
         camera_indices = ray_samples.camera_indices.squeeze()
-        directions = get_normalized_directions(ray_samples.frustums.directions)
+        directions = shift_directions_for_tcnn(ray_samples.frustums.directions)
         directions_flat = directions.view(-1, 3)
         d = self.direction_encoding(directions_flat)
 
         outputs_shape = ray_samples.frustums.directions.shape[:-1]
 
         # appearance
-        embedded_appearance = None
-        if self.embedding_appearance is not None:
-            if self.training:
-                embedded_appearance = self.embedding_appearance(camera_indices)
+        if self.training:
+            embedded_appearance = self.embedding_appearance(camera_indices)
+        else:
+            if self.use_average_appearance_embedding:
+                embedded_appearance = torch.ones(
+                    (*directions.shape[:-1], self.appearance_embedding_dim), device=directions.device
+                ) * self.embedding_appearance.mean(dim=0)
             else:
-                if self.use_average_appearance_embedding:
-                    embedded_appearance = torch.ones(
-                        (*directions.shape[:-1], self.appearance_embedding_dim), device=directions.device
-                    ) * self.embedding_appearance.mean(dim=0)
-                else:
-                    embedded_appearance = torch.zeros(
-                        (*directions.shape[:-1], self.appearance_embedding_dim), device=directions.device
-                    )
-
-        # transients
-        if self.use_transient_embedding and self.training:
-            embedded_transient = self.embedding_transient(camera_indices)
-            transient_input = torch.cat(
-                [
-                    density_embedding.view(-1, self.geo_feat_dim),
-                    embedded_transient.view(-1, self.transient_embedding_dim),
-                ],
-                dim=-1,
-            )
-            x = self.mlp_transient(transient_input).view(*outputs_shape, -1).to(directions)
-            outputs[FieldHeadNames.UNCERTAINTY] = self.field_head_transient_uncertainty(x)
-            outputs[FieldHeadNames.TRANSIENT_RGB] = self.field_head_transient_rgb(x)
-            outputs[FieldHeadNames.TRANSIENT_DENSITY] = self.field_head_transient_density(x)
+                embedded_appearance = torch.zeros(
+                    (*directions.shape[:-1], self.appearance_embedding_dim), device=directions.device
+                )
 
         # semantics
         semantics_input = density_embedding.view(-1, self.geo_feat_dim)
@@ -283,27 +267,35 @@ class FruitProposalField(Field):
         x = self.mlp_semantics(semantics_input).view(*outputs_shape, -1).to(directions)
         outputs[FieldHeadNames.SEMANTICS] = self.field_head_semantics(x)
 
-        # predicted normals
-        if self.use_pred_normals:
-            positions = ray_samples.frustums.get_positions()
-
-            positions_flat = self.position_encoding(positions.view(-1, 3))
-            pred_normals_inp = torch.cat([positions_flat, density_embedding.view(-1, self.geo_feat_dim)], dim=-1)
-
-            x = self.mlp_pred_normals(pred_normals_inp).view(*outputs_shape, -1).to(directions)
-            outputs[FieldHeadNames.PRED_NORMALS] = self.field_head_pred_normals(x)
-
         h = torch.cat(
             [
                 d,
                 density_embedding.view(-1, self.geo_feat_dim),
-            ]
-            + (
-                [embedded_appearance.view(-1, self.appearance_embedding_dim)] if embedded_appearance is not None else []
-            ),
+                embedded_appearance.view(-1, self.appearance_embedding_dim),
+            ],
             dim=-1,
         )
         rgb = self.mlp_head(h).view(*outputs_shape, -1).to(directions)
         outputs.update({FieldHeadNames.RGB: rgb})
 
         return outputs
+
+    def forward(self, ray_samples: RaySamples) -> Dict[FieldHeadNames, Tensor]:
+        """Evaluates the field at points along the ray.
+
+        Args:
+            ray_samples: Samples to evaluate field on.
+        """
+        # Compute density and take last output vector of NN (embedding)
+        density, density_embedding = self.get_density(ray_samples)
+
+        # Get appearance vector and semantic vector
+        if self.test_mode == 'inference' or self.test_mode == 'export':
+            field_outputs = self.get_inference_outputs(ray_samples=ray_samples,
+                                                       density_embedding=density_embedding)
+        else:
+            field_outputs = self.get_outputs(ray_samples=ray_samples,
+                                             density_embedding=density_embedding)
+        field_outputs[FieldHeadNames.DENSITY] = density  # type: ignore
+
+        return field_outputs
